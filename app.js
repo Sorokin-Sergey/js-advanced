641598f6a1edb3a3fd01be4872e09635ee601590
SorokinSergei <sorokin_ser@mail.ru>
2023-07-31 01:16:07 +0400

    Task 1

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+/* Необходимо поменять местами ключи и значения в Map */
+
+const weatherMap = new Map([
+    ['London', 10],
+    ['Moscow', 7],
+    ['Paris', 14],
+]);
+
+const reverseWeatherMap = new Map([...weatherMap].map(el => el.reverse()));
+console.log(reverseWeatherMap);
+
+// console.log(weatherMap);
+// const newArray = [];
+// for (const [key, value] of weatherMap) {
+//     newArray.push([value, key])
+// }
+// const newMap = new Map(newArray);
+// console.log(newMap);
